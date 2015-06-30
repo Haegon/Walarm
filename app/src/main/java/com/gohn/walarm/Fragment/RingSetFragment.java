23e02ff7e1dc5d722a36c226e24986724285dec4
gohn0929 <gohn@ncrewcorp.com>
2015-07-01 00:52:47 +0900

    이것 저것 추가.

--- app/src/main/java/com/gohn/walarm/Fragment/RingSetFragment.java
+++ app/src/main/java/com/gohn/walarm/Fragment/RingSetFragment.java
@@ -2,17 +2,28 @@ package com.gohn.walarm.Fragment;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.gohn.walarm.Activity.AlarmSetActivity;
+import com.gohn.walarm.Extention.TextViewEx;
+import com.gohn.walarm.Model.Flags;
 import com.gohn.walarm.R;
+
+import java.util.ArrayList;
 
 @SuppressLint("ValidFragment")
 public class RingSetFragment extends Fragment {
 	Context mContext;
+	ArrayList<Button> buttons = new ArrayList<Button>();
+	ArrayList<TextViewEx> titles = new ArrayList<TextViewEx>();
+	ArrayList<TextViewEx> rings = new ArrayList<TextViewEx>();
+
 
 	public RingSetFragment(Context context) {
 		mContext = context;
@@ -24,7 +35,36 @@ public class RingSetFragment extends Fragment {
 
 		View view = inflater.inflate(R.layout.fragment_ringset, null);
 
+		buttons.add((Button)view.findViewById(R.id.btn_weather_1));
+		buttons.add((Button)view.findViewById(R.id.btn_weather_2));
+		buttons.add((Button)view.findViewById(R.id.btn_weather_3));
+		buttons.add((Button)view.findViewById(R.id.btn_weather_4));
+		buttons.add((Button)view.findViewById(R.id.btn_weather_5));
+
+		titles.add((TextViewEx)view.findViewById(R.id.text_weather_1));
+		titles.add((TextViewEx)view.findViewById(R.id.text_weather_2));
+		titles.add((TextViewEx)view.findViewById(R.id.text_weather_3));
+		titles.add((TextViewEx)view.findViewById(R.id.text_weather_4));
+		titles.add((TextViewEx)view.findViewById(R.id.text_weather_5));
+
+		rings.add((TextViewEx)view.findViewById(R.id.text_ring_1));
+		rings.add((TextViewEx)view.findViewById(R.id.text_ring_2));
+		rings.add((TextViewEx)view.findViewById(R.id.text_ring_3));
+		rings.add((TextViewEx)view.findViewById(R.id.text_ring_4));
+		rings.add((TextViewEx)view.findViewById(R.id.text_ring_5));
+
 		return view;
 	}
 
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+			case R.id.btn_add_alarm:
+				Intent intent = new Intent(mContext, AlarmSetActivity.class);
+				intent.putExtra(Flags.ALARMSETINTENT, Flags.ADD);
+
+				startActivityForResult(intent, 0);
+				break;
+		}
+	}
 }
